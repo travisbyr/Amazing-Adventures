4b8bbf694b9da1874333ac291101003d1bb1f620
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-03-10 19:13:58 +1300

    Edited fameform

--- amazingAdventures/amazingAdventures/gameForm.Designer.cs
+++ amazingAdventures/amazingAdventures/gameForm.Designer.cs
@@ -31,46 +31,221 @@ namespace amazingAdventures
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.logoutButton = new System.Windows.Forms.Button();
+            this.leaveButton = new System.Windows.Forms.Button();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.highscoreLabel = new System.Windows.Forms.Label();
+            this.totalFoundItemsLabel = new System.Windows.Forms.Label();
+            this.foundItemLabel = new System.Windows.Forms.Label();
+            this.totalPlayersLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Location = new System.Drawing.Point(1423, -2);
+            this.label1.Location = new System.Drawing.Point(1567, -1);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(2, 1011);
+            this.label1.Size = new System.Drawing.Size(2, 1103);
             this.label1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(511, 9);
+            this.label2.Location = new System.Drawing.Point(525, 17);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 47);
             this.label2.TabIndex = 8;
             this.label2.Text = " ";
             // 
-            // logoutButton
+            // leaveButton
             // 
-            this.logoutButton.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.logoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.logoutButton.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.logoutButton.Location = new System.Drawing.Point(1502, 12);
-            this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(282, 60);
-            this.logoutButton.TabIndex = 15;
-            this.logoutButton.Text = "Logout";
-            this.logoutButton.UseVisualStyleBackColor = false;
+            this.leaveButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.leaveButton.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 22F);
+            this.leaveButton.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.leaveButton.Location = new System.Drawing.Point(1623, 20);
+            this.leaveButton.Name = "leaveButton";
+            this.leaveButton.Size = new System.Drawing.Size(266, 51);
+            this.leaveButton.TabIndex = 15;
+            this.leaveButton.Text = "Leave";
+            this.leaveButton.UseVisualStyleBackColor = false;
+            // 
+            // upButton
+            // 
+            this.upButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.upButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.upButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.upButton.Location = new System.Drawing.Point(122, 977);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(258, 46);
+            this.upButton.TabIndex = 16;
+            this.upButton.Text = "Up";
+            this.upButton.UseVisualStyleBackColor = false;
+            // 
+            // downButton
+            // 
+            this.downButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.downButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.downButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.downButton.Location = new System.Drawing.Point(479, 977);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(258, 46);
+            this.downButton.TabIndex = 17;
+            this.downButton.Text = "Down";
+            this.downButton.UseVisualStyleBackColor = false;
+            // 
+            // leftButton
+            // 
+            this.leftButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.leftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.leftButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.leftButton.Location = new System.Drawing.Point(841, 977);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(258, 46);
+            this.leftButton.TabIndex = 18;
+            this.leftButton.Text = "Left";
+            this.leftButton.UseVisualStyleBackColor = false;
+            // 
+            // rightButton
+            // 
+            this.rightButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.rightButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rightButton.Location = new System.Drawing.Point(1183, 977);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(258, 46);
+            this.rightButton.TabIndex = 19;
+            this.rightButton.Text = "Right";
+            this.rightButton.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(629, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(351, 47);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Amazing Adventures";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(52, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1463, 837);
+            this.label4.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(1687, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 37);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Highscore";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(1689, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 37);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Backpack";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(1681, 775);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(170, 37);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Total Players";
+            // 
+            // highscoreLabel
+            // 
+            this.highscoreLabel.AutoSize = true;
+            this.highscoreLabel.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.highscoreLabel.Location = new System.Drawing.Point(1713, 231);
+            this.highscoreLabel.Name = "highscoreLabel";
+            this.highscoreLabel.Size = new System.Drawing.Size(86, 28);
+            this.highscoreLabel.TabIndex = 25;
+            this.highscoreLabel.Text = "12 Items";
+            // 
+            // totalFoundItemsLabel
+            // 
+            this.totalFoundItemsLabel.AutoSize = true;
+            this.totalFoundItemsLabel.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.totalFoundItemsLabel.Location = new System.Drawing.Point(1715, 654);
+            this.totalFoundItemsLabel.Name = "totalFoundItemsLabel";
+            this.totalFoundItemsLabel.Size = new System.Drawing.Size(86, 28);
+            this.totalFoundItemsLabel.TabIndex = 26;
+            this.totalFoundItemsLabel.Text = "12 Items";
+            // 
+            // foundItemLabel
+            // 
+            this.foundItemLabel.AutoSize = true;
+            this.foundItemLabel.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.foundItemLabel.Location = new System.Drawing.Point(1666, 618);
+            this.foundItemLabel.Name = "foundItemLabel";
+            this.foundItemLabel.Size = new System.Drawing.Size(187, 28);
+            this.foundItemLabel.TabIndex = 27;
+            this.foundItemLabel.Text = "You found an apple!";
+            // 
+            // totalPlayersLabel
+            // 
+            this.totalPlayersLabel.AutoSize = true;
+            this.totalPlayersLabel.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.totalPlayersLabel.Location = new System.Drawing.Point(1718, 821);
+            this.totalPlayersLabel.Name = "totalPlayersLabel";
+            this.totalPlayersLabel.Size = new System.Drawing.Size(89, 28);
+            this.totalPlayersLabel.TabIndex = 28;
+            this.totalPlayersLabel.Text = "2 Players";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.Window;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(1661, 398);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(200, 200);
+            this.label8.TabIndex = 29;
             // 
             // gameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1836, 1001);
-            this.Controls.Add(this.logoutButton);
+            this.ClientSize = new System.Drawing.Size(1955, 1069);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.totalPlayersLabel);
+            this.Controls.Add(this.foundItemLabel);
+            this.Controls.Add(this.totalFoundItemsLabel);
+            this.Controls.Add(this.highscoreLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.leaveButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "gameForm";
@@ -84,6 +259,20 @@ namespace amazingAdventures
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button leaveButton;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label highscoreLabel;
+        private System.Windows.Forms.Label totalFoundItemsLabel;
+        private System.Windows.Forms.Label foundItemLabel;
+        private System.Windows.Forms.Label totalPlayersLabel;
+        private System.Windows.Forms.Label label8;
     }
 }
