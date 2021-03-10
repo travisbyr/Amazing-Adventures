7fe798f1e9f803fed9d262d00a59b973ab81e125
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-03-10 18:46:48 +1300

    added gameform

--- amazingAdventures/amazingAdventures/gameForm.Designer.cs
+++ amazingAdventures/amazingAdventures/gameForm.Designer.cs
@@ -29,19 +29,61 @@ namespace amazingAdventures
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logoutButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(1423, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 1011);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(511, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 47);
+            this.label2.TabIndex = 8;
+            this.label2.Text = " ";
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.logoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.logoutButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.logoutButton.Location = new System.Drawing.Point(1502, 12);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(282, 60);
+            this.logoutButton.TabIndex = 15;
+            this.logoutButton.Text = "Logout";
+            this.logoutButton.UseVisualStyleBackColor = false;
             // 
             // gameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1836, 1001);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "gameForm";
             this.Text = "Amazing Adventures - Game";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button logoutButton;
     }
 }
