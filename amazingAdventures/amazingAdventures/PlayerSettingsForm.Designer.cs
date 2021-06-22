070bae0876e26bfabc3de9df43934b6aeb804b14
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-06-22 22:17:41 +1200

    Created another form

--- amazingAdventures/amazingAdventures/PlayerSettingsForm.Designer.cs
+++ amazingAdventures/amazingAdventures/PlayerSettingsForm.Designer.cs
@@ -123,7 +123,7 @@ namespace amazingAdventures
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Tw Cen MT", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(179, 14);
+            this.label1.Location = new System.Drawing.Point(179, 16);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(135, 47);
